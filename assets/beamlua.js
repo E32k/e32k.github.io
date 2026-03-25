308f57c2d9a9814ac799cc8e10bda75844cbc883
E32k <erik32konto@gmail.com>
2026-03-25 21:44:52 +0100

    bruh

--- assets/beamlua.js
+++ assets/beamlua.js
@@ -1,7 +1,7 @@
 // MARK: External Links Class
 document.querySelectorAll('a[href]').forEach(a => {
   if (a.getAttribute('href').startsWith('http'))
-    a.classList.add('ext-link');
+    a.classList.add('link-ext');
 });
 
 
